b6d6c1da6aef0cc4d1b3cf98a24de4d02a47ed07
Khushal Dayala <khushal.dayala@bytestechnolab.com>
2026-04-09 15:19:38 +0530

    fixed

--- public/build/assets/blog-i1_oyTiv.js
+++
@@ -1,1 +0,0 @@
-(function(){new Swiper(".mySwiper8",{autoplay:{delay:3e3,disableOnInteraction:!1},spaceBetween:30,effect:"fade"})})();
--- public/build/assets/form-layout-CdSYHlKe.js
+++
@@ -1,1 +0,0 @@
-(function(){flatpickr("#date",{}),document.addEventListener("DOMContentLoaded",function(){var t=document.querySelectorAll("[data-trigger]");for(let e=0;e<t.length;++e){var a=t[e];new Choices(a,{allowHTML:!0,allowSearch:!1})}})})();
